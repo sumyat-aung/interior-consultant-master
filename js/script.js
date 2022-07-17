7feb39ffd4d1d59309b882c3abea96a06916e3a0
sumyataung2004 <sumyataung1584@gmail.com>
2022-07-18 00:02:55 +0630

    1st commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const icon = document.getElementsByClassName("icon")[0];
+const nav = document.getElementsByTagName("ul")[0];
+
+icon.addEventListener("click", function () {
+  nav.classList.toggle("active");
+  icon.classList.toggle("active");
+});
